dc4bc9492b2e51ac44f04ea090d7716a586d47ce
Susana Piñero <susananzth@gmail.com>
2020-07-31 00:03:36 -0500

    Agregando la funcion para enviar un mensaje cuando se publique un post

--- src/MailController.php
+++ src/MailController.php
@@ -8,4 +8,11 @@
         {
     
         }
+        public function wpdocs_email_friends( $post_id )
+        {
+            $friends = 'bob@example.org, susie@example.org';
+            wp_mail( $friends, "sally's blog updated", 'I just put something on my blog: http://blog.example.com' );
+         
+            return $post_id;
+        }
     }
